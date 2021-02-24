f81514a6bd54521477cc60a6262ee8bf462254a4
jon-immersive <50631505+jon-immersive@users.noreply.github.com>
2021-02-24 16:51:24 +0000

    Title animations update

---
+++ Assets/CloudMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+	public float minX;
+	public float maxX;
+
+	public float speed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate(new Vector3(speed, 0, 0));
+
+		if (transform.position.x < minX)
+		{
+			transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
+		}
+
+		if (transform.position.x > maxX)
+		{
+			transform.position = new Vector3(minX, transform.position.y, transform.position.z);
+		}
+	}
+}
